2a106434f3d7ca105b9036e9cdd5fa64f62995c4
sonsj97 <sonsj97@naver.com>
2024-12-08 00:00:23 +0900

    hi

--- utils/opensearchClient.js
+++ utils/opensearchClient.js
@@ -1,10 +1,15 @@
 import { Client } from '@opensearch-project/opensearch';
 
+// 환경 변수 검증
+if (!process.env.OPENSEARCH_NODE_URL) {
+    throw new Error('Environment variable OPENSEARCH_NODE_URL is missing');
+}
+
 const client = new Client({
-    node: process.env.OPENSEARCH_NODE_URL || 'http://localhost:9200', // 기본 URL
+    node: process.env.OPENSEARCH_NODE_URL, // 필수 옵션
     auth: {
-        username: process.env.OPENSEARCH_USERNAME || 'admin', // 기본 사용자
-        password: process.env.OPENSEARCH_PASSWORD || 'IWantKillRed2689!', // 기본 비밀번호
+        username: process.env.OPENSEARCH_USERNAME || 'default_user', // 선택 사항
+        password: process.env.OPENSEARCH_PASSWORD || 'default_password', // 선택 사항
     },
 });
 
