63d74c9f3ff9d72f8cdff38d19d7d93a737eb4af
sonsj97 <sonsj97@naver.com>
2024-12-07 23:52:41 +0900

    hi

--- utils/opensearchClient.js
+++ utils/opensearchClient.js
@@ -1,11 +1,11 @@
 import { Client } from '@opensearch-project/opensearch';
 
 const client = new Client({
-    node: process.env.OPENSEARCH_NODE_URL,
+    node: process.env.OPENSEARCH_NODE_URL || 'http://localhost:9200', // 기본 URL
     auth: {
-        username: process.env.OPENSEARCH_USERNAME,
-        password: process.env.OPENSEARCH_PASSWORD
-    }
+        username: process.env.OPENSEARCH_USERNAME || 'admin', // 기본 사용자
+        password: process.env.OPENSEARCH_PASSWORD || 'IWantKillRed2689!', // 기본 비밀번호
+    },
 });
 
 export default client;
