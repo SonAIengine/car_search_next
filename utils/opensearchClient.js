01aa6e07ec1adb3d995a7067310204678947e68b
SonAIengine <sonsj97@gmail.com>
2025-03-03 21:26:18 +0900

    Update OpenSearch client configuration with new node URL and credentials

--- utils/opensearchClient.js
+++ utils/opensearchClient.js
@@ -1,16 +1,16 @@
-import { Client } from '@opensearch-project/opensearch';
+import { Client } from "@opensearch-project/opensearch";
 
 // 환경 변수 검증
 if (!process.env.OPENSEARCH_NODE_URL) {
-    throw new Error('Environment variable OPENSEARCH_NODE_URL is missing');
+  throw new Error("Environment variable OPENSEARCH_NODE_URL is missing");
 }
 
 const client = new Client({
-    node: process.env.OPENSEARCH_NODE_URL || 'http://opensearch.infoedu.co.kr:1006', // 필수 옵션
-    auth: {
-        username: process.env.OPENSEARCH_USERNAME || 'admin', // 선택 사항
-        password: process.env.OPENSEARCH_PASSWORD || 'IWantKillRed2689!', // 선택 사항
-    },
+  node: process.env.OPENSEARCH_NODE_URL || "http://http://14.6.96.11:1006/", // 필수 옵션
+  auth: {
+    username: process.env.OPENSEARCH_USERNAME || "admin", // 선택 사항
+    password: process.env.OPENSEARCH_PASSWORD || "Myopensearch!1", // 선택 사항
+  },
 });
 
 export default client;
